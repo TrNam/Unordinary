bb22657e163e1a7799763b56a1ca86327780215f
trnam <trnam2811@gmail.com>
2023-03-13 15:02:36 -0700

    committing to checkout

--- naly/navigation/screens/WorkoutLogScreen.js
+++ naly/navigation/screens/WorkoutLogScreen.js
@@ -1,12 +1,22 @@
 import * as React from 'react';
 import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
 
+// Colors, Fonts and Icons
+import { ICONS } from '../../assets/icons';
+import { COLORS } from '../../assets/colors';
+import { FONTS } from '../../assets/fonts';
+
+const TABS = [
+    { name: '', icon: ''},
+    { name: 'Workouts & Routines', icon: ''},
+    { name: 'Progress', icon: ''},
+]
+
 export default function WorkoutLogScreen({navigation}) {
     return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'#222831'}}>
-            <Text
-            style={{ fontSize: 26, fontWeight: 'bold'}}>Workout Log Screen</Text>
-        </View>
+        <SafeAreaView>
+            
+        </SafeAreaView>
     );
 }
 
